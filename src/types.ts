cce5c2aad09539f1d40ff375117a0563ab09ffbd
Freddy Amarante <freddy@seelo.ch>
2025-05-13 11:49:03 -0400

    fix: product type export

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
-interface Product {
+export interface Product {
   id: number
   title: string
   price: number
   category: string
   image: string
+  rating: {
+    rate: number
+    count: number
+  }
 }
